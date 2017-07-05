fa8fe8377e2cc1d7a171427b8f49e91311dfbb24
SeanyC <sean.conlin@urbanairship.com>
2017-07-05 11:02:43 -0700

    added date to entry. more styling.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('journal')
+
+window.onload = () => {
+  const currentEntryInput = document.getElementById('current-entry')
+  const entryDateElement = document.getElementById('entry-date')
+  currentEntryInput.addEventListener('input', addDateToEntry)
+
+  var entryDate
+  function addDateToEntry (e) {
+    if (e.target.value) {
+      if (!entryDate) {
+        entryDate = new Date()
+        entryDateElement.innerText = entryDate.toLocaleString()
+      }
+    } else {
+      entryDate = ''
+      entryDateElement.innerText = entryDate
+    }
+  }
+}
