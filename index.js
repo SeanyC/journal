e76a2e71fbd58494f3b593565683882934b00aa7
SeanyC <sean.conlin@urbanairship.com>
2017-07-11 16:32:50 -0700

    added entry option link placeholder, removed chrome from electron.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 
 window.onload = () => {
+  const entryContainer = document.getElementById('container')
   const currentEntryInput = document.getElementById('current-entry')
   const entryDateElement = document.getElementById('entry-date')
-  currentEntryInput.addEventListener('input', addDateToEntry)
+  currentEntryInput.addEventListener('input', addEntryTools)
 
   var entryDate
-  function addDateToEntry (e) {
+  var entryOptions
+  function addEntryTools (e) {
     if (e.target.value) {
       if (!entryDate) {
         entryDate = new Date()
         entryDateElement.innerText = entryDate.toLocaleString()
+      }
+      if (!entryOptions) {
+        entryOptions = document.createElement('p')
+        entryOptions.className = 'entry-options'
+        entryOptions.innerHTML = '<a>clear</a> | <a>done</a>'
+        entryContainer.appendChild(entryOptions)
       }
     } else {
       entryDate = ''
